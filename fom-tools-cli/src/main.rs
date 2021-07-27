7aa0078cc3a7b43f2a993d2093ae983499d4c891
Anthony Cramp <anthony.cramp@gmail.com>
2021-07-27 22:54:44 +0930

    CLI app to test library functionality. Will eventually be the front end for the other CLI apps like fom-merge and fom-check.

--- fom-tools-cli/src/main.rs
+++ fom-tools-cli/src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+
+use clap::App;
+use fom_tools_lib;
+use std::fs::File;
+
 fn main() {
     println!("Hello, world!");
+    let netn_base = "modules/NETN-FOM-3.0-rc1/modules/NETN-BASE.xml";
+    if let Ok(netn_base_file) = File::open(netn_base) {
+        let _ = fom_tools_lib::parse(netn_base_file);
+    }
 }
